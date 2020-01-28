8f6a1d018057aaf687013537b07cf5a394d170cf
Timothy Callemein <callemein.timothy@gmail.com>
2020-01-28 08:06:09 +0100

    Create video process script

---
+++ run.py
@@ -0,0 +1,292 @@
+from data import COCODetection, get_label_map, MEANS, COLORS
+from yolact import Yolact
+from utils.augmentations import BaseTransform, FastBaseTransform, Resize
+from utils.functions import MovingAverage, ProgressBar
+from layers.box_utils import jaccard, center_size, mask_iou
+from utils import timer
+from utils.functions import SavePath
+from layers.output_utils import postprocess, undo_image_transformation
+import pycocotools
+
+from data import cfg, set_cfg, set_dataset
+
+import numpy as np
+import torch
+import torch.backends.cudnn as cudnn
+from torch.autograd import Variable
+import argparse
+import time
+import random
+import cProfile
+import pickle
+import json
+import os
+from collections import defaultdict
+from pathlib import Path
+from collections import OrderedDict
+from PIL import Image
+
+import matplotlib.pyplot as plt
+import cv2
+from tqdm import tqdm
+
+def str2bool(v):
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+def parse_args(argv=None):
+    parser = argparse.ArgumentParser(
+        description='YOLACT COCO Evaluation')
+    parser.add_argument('--trained_model',
+                        default='weights/ssd300_mAP_77.43_v2.pth', type=str,
+                        help='Trained state_dict file path to open. If "interrupt", this will open the interrupt file.')
+    parser.add_argument('--top_k', default=5, type=int,
+                        help='Further restrict the number of predictions to parse')
+    parser.add_argument('--cuda', default=True, type=str2bool,
+                        help='Use cuda to evaulate model')
+    parser.add_argument('--fast_nms', default=True, type=str2bool,
+                        help='Whether to use a faster, but not entirely correct version of NMS.')
+    parser.add_argument('--cross_class_nms', default=False, type=str2bool,
+                        help='Whether compute NMS cross-class or per-class.')
+    parser.add_argument('--bbox_det_file', default='results/bbox_detections.json', type=str,
+                        help='The output file for coco bbox results if --coco_results is set.')
+    parser.add_argument('--mask_det_file', default='results/mask_detections.json', type=str,
+                        help='The output file for coco mask results if --coco_results is set.')
+    parser.add_argument('--config', default=None,
+                        help='The config object to use.')
+    parser.add_argument('--video', default=None, type=str,
+                        help='A path to a video to evaluate on. Passing in a number will use that index webcam.')
+    parser.add_argument('--score_threshold', default=0, type=float,
+                        help='Detections with a score under this threshold will not be considered. This currently only works in display mode.')
+
+    global args
+    args = parser.parse_args(argv)
+
+iou_thresholds = [x / 100 for x in range(50, 100, 5)]
+coco_cats = {} # Call prep_coco_cats to fill this
+coco_cats_inv = {}
+color_cache = defaultdict(lambda: {})
+
+
+def prep_coco_cats():
+    """ Prepare inverted table for category id lookup given a coco cats object. """
+    for coco_cat_id, transformed_cat_id_p1 in get_label_map().items():
+        transformed_cat_id = transformed_cat_id_p1 - 1
+        coco_cats[transformed_cat_id] = coco_cat_id
+        coco_cats_inv[coco_cat_id] = transformed_cat_id
+
+
+def get_coco_cat(transformed_cat_id):
+    """ transformed_cat_id is [0,80) as indices in cfg.dataset.class_names """
+    return coco_cats[transformed_cat_id]
+
+
+def get_transformed_cat(coco_cat_id):
+    """ transformed_cat_id is [0,80) as indices in cfg.dataset.class_names """
+    return coco_cats_inv[coco_cat_id]
+
+
+class Detections:
+
+    def __init__(self):
+        self.bbox_data = []
+        self.mask_data = []
+
+    def add_bbox(self, image_id:int, category_id:int, bbox:list, score:float):
+        """ Note that bbox should be a list or tuple of (x1, y1, x2, y2) """
+        bbox = [bbox[0], bbox[1], bbox[2]-bbox[0], bbox[3]-bbox[1]]
+
+        # Round to the nearest 10th to avoid huge file sizes, as COCO suggests
+        bbox = [round(float(x)*10)/10 for x in bbox]
+
+        self.bbox_data.append({
+            'image_id': int(image_id),
+            'category_id': get_coco_cat(int(category_id)),
+            'bbox': bbox,
+            'score': float(score)
+        })
+
+    def add_mask(self, image_id:int, category_id:int, segmentation:np.ndarray, score:float):
+        """ The segmentation should be the full mask, the size of the image and with size [h, w]. """
+        rle = pycocotools.mask.encode(np.asfortranarray(segmentation.astype(np.uint8)))
+        rle['counts'] = rle['counts'].decode('ascii') # json.dump doesn't like bytes strings
+
+        self.mask_data.append({
+            'image_id': int(image_id),
+            'category_id': get_coco_cat(int(category_id)),
+            'segmentation': rle,
+            'score': float(score)
+        })
+    
+    def dump(self):
+        dump_arguments = [
+            (self.bbox_data, args.bbox_det_file),
+            (self.mask_data, args.mask_det_file)
+        ]
+
+        for data, path in dump_arguments:
+            with open(path, 'w') as f:
+                json.dump(data, f)
+    
+    def dump_web(self):
+        """ Dumps it in the format for my web app. Warning: bad code ahead! """
+        config_outs = ['preserve_aspect_ratio', 'use_prediction_module',
+                        'use_yolo_regressors', 'use_prediction_matching',
+                        'train_masks']
+
+        output = {
+            'info' : {
+                'Config': {key: getattr(cfg, key) for key in config_outs},
+            }
+        }
+
+        image_ids = list(set([x['image_id'] for x in self.bbox_data]))
+        image_ids.sort()
+        image_lookup = {_id: idx for idx, _id in enumerate(image_ids)}
+
+        output['images'] = [{'image_id': image_id, 'dets': []} for image_id in image_ids]
+
+        # These should already be sorted by score with the way prep_metrics works.
+        for bbox, mask in zip(self.bbox_data, self.mask_data):
+            image_obj = output['images'][image_lookup[bbox['image_id']]]
+            image_obj['dets'].append({
+                'score': bbox['score'],
+                'bbox': bbox['bbox'],
+                'category': cfg.dataset.class_names[get_transformed_cat(bbox['category_id'])],
+                'mask': mask['segmentation'],
+            })
+
+        with open(os.path.join(args.web_det_path, '%s.json' % cfg.name), 'w') as f:
+            json.dump(output, f)
+
+def runvideo(net: Yolact, path: str, out_path: str = None):
+    # If the input image size is constant, this make things faster (hence why we can use it in a video setting).
+    cudnn.benchmark = True
+
+    vid = cv2.VideoCapture(path)
+
+    if not vid.isOpened():
+        print('Could not open video "%s"' % path)
+        exit(-1)
+
+    target_fps = round(vid.get(cv2.CAP_PROP_FPS))
+    frame_width = round(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_height = round(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    num_frames = round(vid.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    net = net.cuda()
+    transform = torch.nn.DataParallel(FastBaseTransform()).cuda()
+
+    def transform_frame(frames):
+        with torch.no_grad():
+            frames = [torch.from_numpy(frame).cuda().float() for frame in frames]
+            return frames, transform(torch.stack(frames, 0))
+
+    def run_network(inp):
+        with torch.no_grad():
+            frames, imgs = inp
+            out = net(imgs)
+            return frames, out
+
+    video_detections = Detections()
+
+    for frame_idx in tqdm(range(0, 10)):
+        frame = vid.read()[1]
+
+        frames, dets_out = run_network(transform_frame([frame]))
+
+        classes, scores, boxes, masks = postprocess(dets_out, frame_width, frame_height,
+                    score_threshold=args.score_threshold)
+
+        if classes.size(0) == 0:
+            return
+
+        classes = list(classes.cpu().numpy().astype(int))
+
+        if isinstance(scores, list):
+            box_scores = list(scores[0].cpu().numpy().astype(float))
+            mask_scores = list(scores[1].cpu().numpy().astype(float))
+        else:
+            scores = list(scores.cpu().numpy().astype(float))
+            box_scores = scores
+            mask_scores = scores
+
+        masks = masks.view(-1, frame_height * frame_width).cuda()
+        boxes = boxes.cuda()
+
+        boxes = boxes.cpu().numpy()
+        masks = masks.view(-1, frame_height, frame_width).cpu().numpy()
+        for i in range(masks.shape[0]):
+            # Make sure that the bounding box actually makes sense and a mask was produced
+            if (boxes[i, 3] - boxes[i, 1]) * (boxes[i, 2] - boxes[i, 0]) > 0:
+                video_detections.add_bbox(frame_idx, classes[i], boxes[i, :], box_scores[i])
+                video_detections.add_mask(frame_idx, classes[i], masks[i, :, :], mask_scores[i])
+
+        video_detections.dump()
+
+
+    video_detections.dump()
+
+
+
+def run(net: Yolact):
+    net.detect.use_fast_nms = args.fast_nms
+    net.detect.use_cross_class_nms = args.cross_class_nms
+    cfg.mask_proto_debug = False
+
+    # TODO Currently we do not support Fast Mask Re-scroing in evalimage, evalimages, and evalvideo
+    if args.video is not None:
+        if ':' in args.video:
+            inp, out = args.video.split(':')
+            runvideo(net, inp, out)
+        else:
+            runvideo(net, args.video)
+        return
+
+
+if __name__ == '__main__':
+    parse_args()
+
+    if args.config is not None:
+        set_cfg(args.config)
+
+    if args.trained_model == 'interrupt':
+        args.trained_model = SavePath.get_interrupt('weights/')
+    elif args.trained_model == 'latest':
+        args.trained_model = SavePath.get_latest('weights/', cfg.name)
+
+    if args.config is None:
+        model_path = SavePath.from_str(args.trained_model)
+        # TODO: Bad practice? Probably want to do a name lookup instead.
+        args.config = model_path.model_name + '_config'
+        print('Config not specified. Parsed %s from the file name.\n' % args.config)
+        set_cfg(args.config)
+
+    with torch.no_grad():
+        if not os.path.exists('results'):
+            os.makedirs('results')
+
+        if args.cuda:
+            cudnn.fastest = True
+            torch.set_default_tensor_type('torch.cuda.FloatTensor')
+        else:
+            torch.set_default_tensor_type('torch.FloatTensor')
+
+        dataset = None
+
+        print('Loading model...', end='')
+        net = Yolact()
+        net.load_weights(args.trained_model)
+        net.eval()
+        print(' Done.')
+
+        if args.cuda:
+            net = net.cuda()
+
+        prep_coco_cats()
+        run(net)
+
